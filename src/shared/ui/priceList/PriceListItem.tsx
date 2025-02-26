ad1720ff200589d841a65b6f820b367dcf2d5938
unknown <tanechka.kovpak@yandex.ru>
2025-02-26 15:46:43 +0300

    Исправлена ошибка типизации

--- src/shared/ui/priceList/PriceListItem.tsx
+++ src/shared/ui/priceList/PriceListItem.tsx
@@ -10,7 +10,10 @@ export const PriceListItem = ({item}: PriceListItemProps) => {
     return (
         <div className={styles.item}>
             <div className={styles.name}>{item.name}</div>
+            {item.cost && 
             <div>{convertNumberToCost(item.cost)}</div>
+            }
+            
 
         </div>
     );
