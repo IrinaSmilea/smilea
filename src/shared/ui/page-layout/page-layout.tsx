272ca69b3c75e251215eab397506478b03feb043
unknown <tanechka.kovpak@yandex.ru>
2025-08-30 15:33:56 +0300

    метрика тест

--- src/shared/ui/page-layout/page-layout.tsx
+++ src/shared/ui/page-layout/page-layout.tsx
@@ -1,11 +1,22 @@
 import {PropsWithChildren} from "react";
 import classNames from "classnames";
+import { YMInitializer } from 'react-yandex-metrika';
 
 import styles from "./page-layout.module.css";
 
 export const PageLayout = ({children}: PropsWithChildren) => {
     return (
         <main className={classNames("container", styles.layout)}>
+            <YMInitializer
+        accounts={[103953677]} // ваш ID счетчика Яндекс.Метрики
+        options={{
+          webvisor: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          clickmap: true
+        }}
+        version="2"
+      />
             {children}
         </main>
     );
